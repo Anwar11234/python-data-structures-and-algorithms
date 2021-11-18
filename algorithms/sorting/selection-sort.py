094227b7e0f58aa67e915a47374278a54c62907d
Anwar11234 <ahmedanwar8910@gmail.com>
2021-11-18 06:57:20 +0200

    modified selection sort

--- algorithms/sorting/selection-sort.py
+++ algorithms/sorting/selection-sort.py
@@ -1,6 +1,6 @@
 def selection_sort(alist):
     n = len(alist)
-    for i in range(n):
+    for i in range(n - 1):
         min_index = i
         for j in range(i + 1 , n):
             if alist[j] < alist[min_index]:
