231c678e839700c2e24138538502c60afd53e09d
Anwar11234 <ahmedanwar8910@gmail.com>
2021-11-18 06:27:17 +0200

    fixed recursive binary search

--- algorithms/searching/binary-search.py
+++ algorithms/searching/binary-search.py
@@ -1,4 +1,4 @@
-def binary_seacrh(list , target):
+def binary_search(list , target):
     low = 0
     high = len(list) - 1
     while low <= high:
@@ -21,6 +21,10 @@ def recursive_binary_search(list , target):
     if list[mid] == target:
         return True
     elif list[mid] > target:
-        return recursive_binary_search(list[mid+1:] , target)
+        return recursive_binary_search( list[:mid] , target)
     else:
-        return recursive_binary_search(list[:mid] , target)        
+        return recursive_binary_search(list[mid+1:] , target)        
+
+testlist = [0, 1, 2, 8, 13, 17, 19, 32, 42,]
+print(binary_search(testlist, 3))
+print(binary_search(testlist, 13))        
